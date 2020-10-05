bf14d931a64984d596b3c40dd882188f76651365
Maycie Morris <maycie.morris@gmail.com>
2020-10-05 18:01:48 -0400

    CRUD complete

--- api/server.js
+++ api/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+// const db = require("../data/dbConfig.js");
 
 const server = express();
 
+
 server.use(express.json());
 
+const AccountsRouter = require('../accounts/accounts-router')
+
+
+server.get('/', (req, res) => {
+    // res.status(200).json({ api: "up" })
+    res.send(`<h1>Up</h1>`)
+})
+
+server.use('/api/accounts', AccountsRouter)
+
 module.exports = server;
+
